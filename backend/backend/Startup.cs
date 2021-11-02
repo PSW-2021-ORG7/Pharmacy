a322b02006ebe59aa240dbaf580ea7cefc968c00
StefanLjubovic <ljubovicstefan@gmail.com>
2021-11-02 22:21:48 +0100

    Init project

---
+++ backend/backend/Startup.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft;
+using Newtonsoft.Json.Serialization;
+using backend.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using backend.Repositories.Interfaces;
+using backend.Repositories;
+
+namespace backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Enable CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+            //JSON Serializer
+            
+            services.AddControllers().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+        .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            //EntityFramework 
+            services.AddDbContext<DrugStoreContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DrugstoreCon")));
+
+            services.AddControllers();
+
+            //Dependency injection
+
+            services.AddScoped<IMedicineRepository, MedicineRepository>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            //Enable CORS
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
