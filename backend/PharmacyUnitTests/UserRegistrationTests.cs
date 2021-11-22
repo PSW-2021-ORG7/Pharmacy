435131848e36fe47e26387824dd9722771aaeed2
Marija Klještan <marijakljestan@uns.ac.rs>
2021-11-22 19:01:31 +0100

    test: Added unit tests for registration and login

---
+++ backend/PharmacyUnitTests/UserRegistrationTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using backend.Model;
+using backend.Repositories.Interfaces;
+using backend.Services;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace PharmacyUnitTests
+{
+    public class UserRegistrationAndLoginTests
+    {
+        [Fact]
+        public void Register_user_with_same_username()
+        {
+            var stubRepository = CreateStubRepository();
+            UserService service = new UserService(stubRepository.Object);
+            User pera = CreateUser("pera", "123");
+
+            bool registrated = service.RegisterUser(pera);
+
+            registrated.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void Register_user_with_unique_username()
+        {
+            var stubRepository = CreateStubRepository();
+            UserService service = new UserService(stubRepository.Object);
+            User mika = CreateUser("mika", "123");
+
+            bool registrated = service.RegisterUser(mika);
+
+            registrated.ShouldBeTrue();
+        }
+
+        private static Mock<IUserRepository> CreateStubRepository()
+        {
+            var stubRepository = new Mock<IUserRepository>();
+            var users = new List<User>();
+            User pera = CreateUser ("pera", "pera123");
+            users.Add(pera);
+
+            stubRepository.Setup(x => x.GetAll()).Returns(users);
+
+            return stubRepository;
+        }
+
+        private static User CreateUser (string username, string password)
+        {
+            User user = new User();
+            user.FirstName = "name";
+            user.LastName = "surname";
+            user.Username = username;
+            user.Password = password;
+            user.Role = User.UserRole.Client;
+
+            return user;
+        }
+    }
+}
--- backend/backend/Services/UserService.cs
+++ backend/backend/Services/UserService.cs
@@ -1,8 +1,6 @@
 ﻿using backend.DTO;
-using backend.Helpers;
 using backend.Model;
 using backend.Repositories.Interfaces;
-using Microsoft.Extensions.Options;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,12 +9,10 @@ namespace backend.Services
     public class UserService
     {
         private readonly IUserRepository _userRepository;
-        private readonly AppSettings _appSettings;
 
-        public UserService(IUserRepository userRepository, IOptions<AppSettings> appSettings)
+        public UserService(IUserRepository userRepository)
         {
             _userRepository = userRepository;
-            _appSettings = appSettings.Value;
         }
 
         public List<User> GetAll()
@@ -30,32 +26,26 @@ namespace backend.Services
 
         }
 
-        public UserLoginRequestDTO GetUserCredentials (string username)
-        {
-            UserLoginRequestDTO credentials = new UserLoginRequestDTO();
-            credentials.Username = username;
-            credentials.Password = GetUserByUsername(username).Password;
-            return credentials;
-        }
-
         public bool RegisterUser(User newUser)
         {
+            /*
             if (_userRepository.Save(newUser))
                 return true;
 
             return false;
-        }
+            */
 
-        public bool IsValidUserLoginData(UserLoginRequestDTO userDTO)
-        {
-            User user = _userRepository.GetAll().SingleOrDefault(u => u.Username == userDTO.Username && u.Password == userDTO.Password);
-
-            if (user == null)
+            if (_userRepository.GetAll().Any(u => u.Username == newUser.Username))
                 return false;
 
+            _userRepository.Save(newUser);
             return true;
-
         }
 
+        public User GetUserByLoginCredentials(UserLoginRequestDTO userDTO)
+        {
+            User user = _userRepository.GetAll().SingleOrDefault(u => u.Username == userDTO.Username && u.Password == userDTO.Password);
+            return user;
+        }
     }
 }
