61544999ce6a73477e95fba1a7cdabe4059b6573
Dejan Bjelic <iodum99@yahoo.com>
2022-01-09 21:44:51 +0100

    refactor: Adjusted mapping and parameters

--- backend/backend/DTO/TenderingDTO/TenderingOfferDTO.cs
+++ backend/backend/DTO/TenderingDTO/TenderingOfferDTO.cs
@@ -8,6 +8,8 @@ namespace backend.DTO
 {
     public class TenderingOfferDTO
     {
+        public String TenderKey { get; set; }
+        public String ApiKey { get; set; }
         public List<TenderingOfferItemDTO> tenderingOfferItems { get; set; }
         public String PriceForAllAvailable { get; set; }
         public String PriceForAllRequired { get; set; }
